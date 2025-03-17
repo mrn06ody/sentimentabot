d79f5c8cf49bc23604447ce368b7a00546dd37dc
Azhdar <azhdarshirinzada@yandex.ru>
2025-03-18 00:10:56 +0400

    finally find a way to get all channel post replies

--- Program.cs
+++ Program.cs
@@ -30,18 +30,16 @@ bot.OnMessage += OnMessage;
 while (Console.ReadKey(true).Key != ConsoleKey.Escape) { }
 
 
-async Task OnMessage(WTelegram.Types.Message msg, UpdateType type)
+async Task OnMessage(WTelegram.Types.Message message, UpdateType type)
 {
-    // commands accepted:
-    if (msg.Text == "/start")
+    if (message.Text == "/start")
     {
-        //---> It's easy to reply to a message by giving its id to replyParameters: (was broken in Telegram.Bot v20.0.0)
-        await bot.SendMessage(msg.Chat, $"Hello, {msg.From}!\nTry commands /pic /react /lastseen /getchat /setphoto", replyParameters: msg);
+        await bot.SendMessage(message.Chat, $"Hello, {message.From}!\nTry commands /pic /react /lastseen /getchat /setphoto", replyParameters: message);
         return;
     }
 
 
-    if (msg.ForwardOrigin is not MessageOriginChannel originChannel)
+    if (message.ForwardOrigin is not MessageOriginChannel originChannel)
     {
         return;
     }
@@ -51,33 +49,37 @@ async Task OnMessage(WTelegram.Types.Message msg, UpdateType type)
         await bot.Client.LoginUserIfNeeded();
 
         var chatId = long.Parse(originChannel.Chat.Id.ToString().Replace("-100", string.Empty));
-        var chat = await bot.Client.Messages_GetChats(chatId);
-        var test = await bot.Client.Messages_GetDiscussionMessage(chat.chats[chatId], msg.TLMessage.ID);
+        var chats = await bot.Client.Channels_GetChannels(new InputChannel(chatId, 0));
+
+
+        var peer = chats.chats[chatId];
+
+        var replies = await bot.Client.Messages_GetReplies(peer, (message.TLMessage as TL.Message).fwd_from.channel_post);
     }
     catch (Exception)
     {
     }
 }
 
-using var client = new WTelegram.Client(Config);
-var myself = await client.LoginUserIfNeeded();
-Console.WriteLine($"We are logged-in as {myself} (id {myself.id})");
+//using var client = new WTelegram.Client(Config);
+//var myself = await client.LoginUserIfNeeded();
+//Console.WriteLine($"We are logged-in as {myself} (id {myself.id})");
 
 
-var chats = await client.Messages_GetAllChats();
-InputPeer peer = chats.chats[1482890345]; // the chat (or User) we want
-                                          //for (int offset_id = 0; ;)
-                                          //{
-                                          //    var messages = await client.Messages_GetHistory(peer, offset_id);
-                                          //    if (messages.Messages.Length == 0) break;
-                                          //    foreach (var msgBase in messages.Messages)
-                                          //    {
-                                          //        var test = await client.Messages_GetDiscussionMessage(peer, msgBase.ID);
-                                          //        var test2 = (test.messages[0] as Message);
-                                          //        var test3 = await client.Messages_GetReplies(peer, msgBase.ID);
+//var chats = await client.Messages_GetAllChats();
+//InputPeer peer = chats.chats[1482890345]; // the chat (or User) we want
+//for (int offset_id = 0; ;)
+//{
+//    var messages = await client.Messages_GetHistory(peer, offset_id);
+//    if (messages.Messages.Length == 0) break;
+//    foreach (var msgBase in messages.Messages)
+//    {
+//        var test = await client.Messages_GetDiscussionMessage(peer, msgBase.ID);
+//        var test2 = (test.messages[0] as TL.Message);
+//        var test3 = await client.Messages_GetReplies(peer, msgBase.ID);
 
 //        var from = messages.UserOrChat(msgBase.From ?? msgBase.Peer); // from can be User/Chat/Channel
-//        if (msgBase is Message msg)
+//        if (msgBase is TL.Message msg)
 //            Console.WriteLine($"{from}> {msg.message} {msg.media}");
 //        else if (msgBase is MessageService ms)
 //            Console.WriteLine($"{from} [{ms.action.GetType().Name[13..]}]");
@@ -94,9 +96,10 @@ string? Config(string what)
         case "bot_token": return config.GetValue<string>("BotToken");
         case "phone_number": return config.GetValue<string>("PhoneNumber");
         case "verification_code": Console.Write("Code: "); return Console.ReadLine();
-        case "first_name": return "John";      // if sign-up is required
-        case "last_name": return "Doe";        // if sign-up is required
-        case "password": return config.GetValue<string>("Password");     // if user has enabled 2FA
-        default: return null;                  // let WTelegramClient decide the default config
+        case "first_name": return "Azhdar";
+        case "last_name": return "Shirinzada";
+        case "password": return config.GetValue<string>("Password");
+        case "server_address": return "2>149.154.167.40:443"; // test DC
+        default: return null; // let WTelegramClient decide the default config
     }
 }
